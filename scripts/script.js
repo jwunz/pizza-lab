a025670b481be4c944819c1356a092d9a0fc541d
Matthew Guernsey <guernsey624@gmail.com>
2018-05-29 09:08:35 -0600

    added javascript

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+var pizzaBackground = document.createElement("div");
+pizzaBackground.setAttribute("id", "pizza-background");
+pizzaBackground.style.background = "url(../images/crust_background.png)";
+pizzaBackground.style.height = "500px";
+
+document.getElementById("pizza").appendChild(pizzaBackground);

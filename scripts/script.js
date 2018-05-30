c2d867e138cb4db5f29e18649bc2cbead4281a78
Matthew Guernsey <guernsey624@gmail.com>
2018-05-29 18:07:30 -0600

    removed unnecessary images, and updates to the script, also added side images for jalapenos, more to follow.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,23 @@
+var pizzaDiv = document.getElementById('pizza');
+
 var pizzaBackground = document.createElement("div");
 pizzaBackground.setAttribute("id", "pizza-background");
-pizzaBackground.style.background = "url(../images/crust_background.png)";
-pizzaBackground.style.height = "500px";
+pizzaBackground.style.background = "url(../images/crust_background.png) no-repeat";
+pizzaDiv.appendChild(pizzaBackground);
 
-document.getElementById("pizza").appendChild(pizzaBackground);
+var baconBtn = document.createElement("div");
+baconBtn.setAttribute("id", "bacon-button");
+baconBtn.style.background = "#fab";
+baconBtn.style.width = "100px";
+baconBtn.style.display = "flex";
+baconBtn.addEventListener(addTopping);
+
+document.getElementById("pizza").insertBefore(baconBtn, pizzaBackground);
+
+function toppingLeftHalfOnly(image){
+    
+}
+
+function addTopping(evt){
+    pizzaBackground.style.background = "url(../images/bacon_regular.png), url(../images/crust_background.png)"
+}

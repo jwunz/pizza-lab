f6d9103ed9061bc260dbeb150866ba6e1467eaa9
Joseph Wunz <jodaman19@gmail.com>
2018-06-05 16:52:00 -0600

    *Radio Button shit again...

--- scripts/script.js
+++ scripts/script.js
@@ -17,7 +17,6 @@ loadData();
 
 
 function loadData(){
-    console.log('Link Start');
     request.open('GET', '../data/toppings.json');
     request.onload = loadComplete;
     request.send();
@@ -26,7 +25,7 @@ function loadComplete(evt){
     toppingContent = JSON.parse(request.responseText);
     toppings = toppingContent.toppings;
     for(i = 0; i < toppings.length; i++) {
-        toppingMenuContent.innerHTML += '<div class="topping"> <div class="topping-display"> <img class="topping-img" src="../images/' + toppings[i].thumbnail + '"/>  <div class="topping-text">' + toppings[i].name + '</div> </div> <div class="topping-controls"> <div class="topping-control-label">Regular</div> <div class="half-selection-container regular-half-selection-container"> <div class="half-or-whole left-half-btn"></div> <div class="half-or-whole whole-btn"></div> <div class="half-or-whole right-half-btn"></div> </div><div class="topping-control-label">Extra</div> <div class="half-selection-container extra-half-selection-container"> <div class="half-or-whole left-half-btn"></div> <div class="half-or-whole whole-btn"></div> <div class="half-or-whole right-half-btn"></div> </div> </div> </div > ';
+        toppingMenuContent.innerHTML += `<div class="topping ${toppings[i].name}"> <div class="topping-display"> <img class="topping-img" src="../images/${toppings[i].thumbnail}"/>  <div class="topping-text">${toppings[i].name}</div> </div> <div class="topping-controls"> <div class="topping-control-label">Regular</div> <div class="half-selection-container regular-half-selection-container"> <div class="half-or-whole left-half-btn"></div> <div class="half-or-whole whole-btn"></div> <div class="half-or-whole right-half-btn"></div> </div><div class="topping-control-label">Extra</div> <div class="half-selection-container extra-half-selection-container"> <div class="half-or-whole left-half-btn"></div> <div class="half-or-whole whole-btn"></div> <div class="half-or-whole right-half-btn"></div> </div> </div> </div >`;
     }
 
     halfSelectors = document.getElementsByClassName('half-or-whole');
@@ -58,12 +57,49 @@ function showStarters(evt) {
 function activateHalf(evt) {
     if(!evt.target.classList.contains('active')){
         if(evt.target.parentElement.classList.contains('regular-half-selection-container')) {
-            deactivateArray(evt.target.parentElement.parentElement.children[3].children);
+            if (evt.target.classList.contains('whole-btn')) {
+                deactivateArray(evt.target.parentElement.parentElement.children[3].children);
+            }
+            else if (evt.target.classList.contains('left-half-btn')) {
+                if(evt.target.parentElement.parentElement.children[3].children[0].classList.contains('active')){
+                    deactivate(evt.target.parentElement.parentElement.children[3].children[0]);
+                }
+                else if(evt.target.parentElement.parentElement.children[3].children[1].classList.contains('active')){
+                    deactivate(evt.target.parentElement.parentElement.children[3].children[1]);
+                    activate(evt.target.parentElement.parentElement.children[3].children[2])
+                }
+            }
+            else {
+                if(evt.target.parentElement.parentElement.children[3].children[2].classList.contains('active')){
+                    deactivate(evt.target.parentElement.parentElement.children[3].children[2]);
+                }
+                else if(evt.target.parentElement.parentElement.children[3].children[1].classList.contains('active')){
+                    deactivate(evt.target.parentElement.parentElement.children[3].children[1]);
+                    activate(evt.target.parentElement.parentElement.children[3].children[0])
+                }
+            }
         } 
         else if (evt.target.parentElement.classList.contains('extra-half-selection-container')) {
             if (evt.target.classList.contains('whole-btn')) {
-                console.log(evt.target.parentElement.parentElement.children[1].children);
                 deactivateArray(evt.target.parentElement.parentElement.children[1].children);
+            }
+            else if (evt.target.classList.contains('left-half-btn')) {
+                if(evt.target.parentElement.parentElement.children[1].children[0].classList.contains('active')){
+                    deactivate(evt.target.parentElement.parentElement.children[1].children[0]);
+                }
+                else if(evt.target.parentElement.parentElement.children[1].children[1].classList.contains('active')){
+                    deactivate(evt.target.parentElement.parentElement.children[1].children[1]);
+                    activate(evt.target.parentElement.parentElement.children[1].children[2])
+                }
+            }
+            else {
+                if(evt.target.parentElement.parentElement.children[1].children[2].classList.contains('active')){
+                    deactivate(evt.target.parentElement.parentElement.children[1].children[2]);
+                }
+                else if(evt.target.parentElement.parentElement.children[1].children[1].classList.contains('active')){
+                    deactivate(evt.target.parentElement.parentElement.children[1].children[1]);
+                    activate(evt.target.parentElement.parentElement.children[1].children[0])
+                }
             }
         }
 

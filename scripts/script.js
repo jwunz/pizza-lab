ca17e67adc701f4b1ee0748c7df4d483d76626a7
Matthew Guernsey <guernsey624@gmail.com>
2018-06-06 02:02:22 -0600

    Pizza Preview is working

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,8 @@ var toppingMenuContent = document.getElementById("toppings-menu-content");
 var pizzaPreview = document.getElementsByClassName('pizza-preview')[0];
 var halfSelectors;
 var halfSelectorContainers;
-var activePizzaToppings;
+var activePizzaToppings = [];
+var PIZZABACKGROUND;
 var toppingClickables;
 
 toppingTab.onclick = showToppings;
@@ -34,13 +35,14 @@ function loadComplete(evt){
     for(i = 0; i < toppings.length; i++) {
         toppingMenuContent.innerHTML += `<div class="topping ${toppings[i].name}"> <div class="topping-display"> <img class="topping-img" src="../images/${toppings[i].thumbnail}"/>  <div class="topping-text">${toppings[i].name}</div> </div> <div class="topping-controls"> <div class="topping-control-label">Regular</div> <div class="half-selection-container regular-half-selection-container"> <div class="half-or-whole left-half-btn"></div> <div class="half-or-whole whole-btn"></div> <div class="half-or-whole right-half-btn"></div> </div><div class="topping-control-label">Extra</div> <div class="half-selection-container extra-half-selection-container"> <div class="half-or-whole left-half-btn"></div> <div class="half-or-whole whole-btn"></div> <div class="half-or-whole right-half-btn"></div> </div> </div> </div >`;
     }
-    
-    pizzaPreview.style.background = `url(../images/${cheese.image}) no-repeat, url(../images/${crust.image[0]}) no-repeat, url(../images/${crust.image[1]}) no-repeat`;
+    PIZZABACKGROUND =  `url(../images/${cheese.image}) no-repeat, url(../images/${crust.image[0]}) no-repeat, url(../images/${crust.image[1]}) no-repeat`;
+    activePizzaToppings.push(PIZZABACKGROUND);
+    pizzaPreview.style.background = PIZZABACKGROUND;
     pizzaPreview.style.backgroundSize = 'cover';
     halfSelectors = document.getElementsByClassName('half-or-whole');
     halfSelectorContainers = document.getElementsByClassName('half-selection-container');
     toppingClickables = document.getElementsByClassName('topping-display');
-
+    
     for(var i = 0; i < halfSelectors.length; i++) {
         halfSelectors[i].onclick = activateHalf;
     }
@@ -72,7 +74,6 @@ function showStarters(evt) {
 }
 
 function activateHalf(evt) {
-    console.log(evt);
     if(!evt.target.classList.contains('active')){
         if(evt.target.parentElement.classList.contains('regular-half-selection-container')) {
             if (evt.target.classList.contains('whole-btn')) {
@@ -129,7 +130,6 @@ function activateHalf(evt) {
 }
 
 function toppingClick(evt) {
-    console.log(evt);
     var greatGrandchildren1 = evt.target.parentElement;
     var greatGrandchildren2 = evt.target.parentElement;
 
@@ -152,14 +152,50 @@ function toppingClick(evt) {
 
 function activate(element) {
     element.classList.add('active');
+    var topp = element.parentElement.parentElement.parentElement.classList[1];
+    var picturePath = findPicturePath(element);
+    var topp2 = topp.toLowerCase();
+    activePizzaToppings.unshift(`url(../images/${topp2}${picturePath}.png) no-repeat`);
+    console.log(activePizzaToppings);
+    pizzaPreview.style.background = activePizzaToppings;
+    pizzaPreview.style.backgroundSize = "cover";
 }
 
 function deactivate(element) {
-    element.classList.remove('active');
+    if(element.classList.contains('active')){
+
+        element.classList.remove('active');
+        var topp = element.parentElement.parentElement.parentElement.classList[1];
+        var picturePath = findPicturePath(element);
+        var topp2 = topp.toLowerCase();
+        var index = activePizzaToppings.indexOf(`url(../images/${topp2}${picturePath}.png) no-repeat`);
+        activePizzaToppings.splice(index,1);
+        console.log(activePizzaToppings);
+        pizzaPreview.style.background = activePizzaToppings;
+        pizzaPreview.style.backgroundSize = "cover";
+    }
 }
 
 function deactivateArray(arr){
     for(var i = 0; i < arr.length; i++){
         deactivate(arr[i]);
+    }
+}
+
+
+function findPicturePath(element){
+    var picturePath;
+    var parentClassName = element.parentElement.classList[1];
+    if(parentClassName.includes('regular')){
+        picturePath="_regular";
+        if(element.classList[1].includes('left')) picturePath+="_left";
+        else if(element.classList[1].includes('right')) picturePath+="_right";
+        return picturePath;
+    }
+    else if(parentClassName.includes('extra')){
+        picturePath="_extra";
+        if(element.classList[1].includes('left')) picturePath+="_left";
+        else if(element.classList[1].includes('right')) picturePath+="_right";
+        return picturePath;
     }
 }
